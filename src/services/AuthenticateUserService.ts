1db702937cdfa7b5181b362c3402998435ce84ad
carlinxoldz <carlinxold@hotmail.com>
2021-07-16 12:21:01 -0300

    terceiro

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,3 +1,7 @@
+import { getCustomRepository } from 'typeorm';
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
+import { UsersRepositories } from '../repositories/UsersRepositories';
 
 
 
@@ -7,7 +11,36 @@ interface IAuthenticateRequest {
 }
 
 class AuthenticateUserService {
-    async execute() {
+    async execute({email, password}: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        //verificar se o email existe
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect");
+            
+        }
+
+        //verificar se a senha está correta
+        const passwordMath = await compare(password, user.password);
+
+        if (!passwordMath) {
+            throw new Error("Email/Password incorrect");
+            
+        }
+
+        //gerar token
+        const token = sign({
+            email: user.email
+        }, "294930ccmdfs2j4j5554l32l1xxddj", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        return token;
 
     }
 }
